bbc166015ec5937a84763dc6b31c0dcbc7ca0a6e
LUIS HENRIQUE ABRANCHES <luisabranches.surf@hotmail.com>
2026-05-13 01:03:54 -0300

    Update CoremetricsApplication.java

--- backend/src/main/java/com/coremetrics/core/CoremetricsApplication.java
+++ backend/src/main/java/com/coremetrics/core/CoremetricsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class CoremetricsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmetaApplication.class, args);
+		SpringApplication.run(CoremetricsApplication.class, args);
 	}
 
 }
